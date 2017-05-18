1e2a459f58e272c8ab9668a937fd9a5db0dfe890
jzucchetto <jzucchetto@gmail.com>
2017-05-18 01:30:31 -0700

    randomize interval

--- TrafficGen/Program.cs
+++ TrafficGen/Program.cs
@@ -43,7 +43,7 @@ namespace TrafficGenerator
                 if (interval != null)
                     i = Convert.ToInt32(interval);
 
-                Thread.Sleep(i);
+                Thread.Sleep(rnd.Next(100, 100 + i));
             }
         }
 
